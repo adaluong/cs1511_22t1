75709fd3b2f57312b92f971111a964f8af931286
adaluong <35322993+adaluong@users.noreply.github.com>
2022-02-15 22:58:02 +1100

    files first tut01

---
+++ W09B/tut01/face.c
@@ -0,0 +1,15 @@
+// Printing a face with double backslashes
+// Demonstration of escape sequences
+// Ada Luong, February 2022
+
+#include <stdio.h>
+
+int main (void) {
+
+    // ~ ~
+    // 0 0
+    //  o
+    //  -
+
+	return 0;
+}
---
+++ W09B/tut01/hello_world.c
@@ -0,0 +1,11 @@
+// Saying hi to the world
+// by Ada Luong, Februrary 2022
+
+#include <stdio.h>
+
+int main (void) {
+	
+    printf("hello, world!\n");
+
+    return 0;
+}
