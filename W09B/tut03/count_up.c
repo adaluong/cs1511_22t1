acd18a233d41f074bc396df3d0bbcbd8169324ef
adaluong <35322993+adaluong@users.noreply.github.com>
2022-03-02 10:18:57 +1100

    Add tut03 files

--- W09B/tut03/count_up.c
+++ W09B/tut03/count_up.c
@@ -2,13 +2,21 @@
 // Program reads an integer n and then prints the integers 1..n, one per line
 // Ada Luong, March 2022
 
+#include <stdio.h>
+
 int main(void) {
 
-    int n;
+    int end_number;
     printf("Enter finish: ");
-    scanf("%d", &n);
+    scanf("%d", &end_number);
 
-    
+    int current_number = 1;
+    while(current_number <= end_number) {
+
+        printf("%d\n", current_number);
+
+        current_number = current_number + 1;
+    }
 
     return 0;
 }
