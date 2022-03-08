cdee8a6e6e5465aec9e34fceee802e44fdad14fd
adaluong <35322993+adaluong@users.noreply.github.com>
2022-03-08 21:15:47 +1100

    Add tut04 stubs

---
+++ W09B/tut04/arrays.c
@@ -0,0 +1,40 @@
+// COMP1511 Tut04 - initialising and printing an array demo
+// Ada Luong, March 2022
+
+#include <stdio.h>
+
+int main (void) {
+
+    int num1 = 1;
+    int num2 = 2;
+    int num3 = 3;
+    int num4 = 4;
+    int num5 = 5;
+
+    // declaring an array and initialising every element to zero 
+
+    // declaring and initialising an array of size 5
+
+    // declaring an array and initialising every element to one? 
+
+    // printing every element of an array
+
+    // what's the issue with accessing the array[5]
+    // printf("%d", numbers[5]);
+    // try gcc -o arrays arrays.c --> won't throw an error which is BAD
+
+    return 0;
+}
+
+// Function to print the elements of an array
+
+
+/* THINGS TO KEEP NOTE OF
+ * Declaring variable type
+ * Naming the array
+ * Size of the array
+ * Initialising elements
+ * Initialising all elements to zero 
+ * Printing elements of an array
+ * Printing in a function - the length parameter
+ */
---
+++ W09B/tut04/is_even.c
@@ -0,0 +1,10 @@
+// COMP1511 Tut04 - function basics
+// A function which takes in an integer and returns 1 if it is an even 
+// number and 0 if it is not
+
+#include <stdio.h>
+
+int main (void) {
+
+    return 0;
+}
