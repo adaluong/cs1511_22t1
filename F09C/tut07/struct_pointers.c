7c23d3662098dc27cc2e004a9bc4c97e46846a07
z5358341 <z5358341@cse.unsw.edu.au>
2022-04-01 11:51:20 +1100

    struct_pointers.c

--- F09C/tut07/struct_pointers.c
+++ F09C/tut07/struct_pointers.c
@@ -1,27 +1,31 @@
-// COMP1511 Tut07 - Struct Pointers
-// Ada Luong, March 2022 
+// Struct Pointers
 
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_NAME_LENGTH 256
+#define MAX_NAME_LENGTH 20
 
+// Struct student definition
 struct student {
-    int zID;
-    double wam;
-    char name[MAX_NAME_LENGTH];
+  int zID;
+  double wam;
+  char name[MAX_NAME_LENGTH];
 };
 
 int main(void) {
 
-    // create a variable `frankie` which is a `struct student`
+    // Declare a variable named stu of type struct student
+    struct student stu;
 
-    // create a variable `frankie_pointer` that points to this new struct
+    // Declate a pointer to my student struct
+    struct student *stu_pointer = &stu;
 
-    // and change frankie's wam to 100
-    
-    // what is the use of the -> operator? change the previous code to use it
+    stu_pointer->zID = 5123456;     // instead of (*stu_pointer).zID = 5123456;
+    stu_pointer->wam = 74.7;
+    strcpy(stu_pointer->name, "Frankie");
 
+    // Print statement for debugging
+    printf("%d, %.2lf, %s\n", stu.zID, stu.wam, stu.name);
     return 0;
 }
 
