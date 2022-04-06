a8f8ba72f5e311a216a109670e751b7dad1f9c01
adaluong <35322993+adaluong@users.noreply.github.com>
2022-04-06 10:09:43 +1000

    Add tut08 files

--- W09B/tut08/linked_lists.c
+++ W09B/tut08/linked_lists.c
@@ -8,24 +8,52 @@
 #define MAX_NAME_LENGTH 256
 
 struct student *create_student(int zID, double mark, char *name);
+void print_all_students(struct student *first_student);
 
 struct student {
     int zID;
     double mark;
     char name[MAX_NAME_LENGTH];
+    struct student *next_student;
 };
 
 int main(void) {
 
+    struct student *gary = create_student(54321, 100, "gary");
+    struct student *lara = create_student(12345, 100, "lara");
+    struct student *john = create_student(00000, 100, "john");
+    struct student *ada = create_student(11111, 100, "ada");
+    struct student *joel = create_student(22222, 100, "joel");
+
+    gary->next_student = lara;
+    lara->next_student = john;
+    john->next_student = ada;
+    ada->next_student = joel;
+    joel->next_student = NULL;
+
+    print_all_students(gary);
+
     return 0;
 }
 
-// A function that creates a new struct student
+// Function to print all students given the first node of the list
+void print_all_students(struct student *first_student) {
+    // creating a separate pointer to keep track of which student
+    // we are printing (note: no malloc here!)
+    struct student *current_student_ptr = first_student;
+    while (current_student_ptr != NULL) {
+        printf("name: %s\n", current_student_ptr->name);
+        current_student_ptr = current_student_ptr->next_student; 
+    }
+}
+
+// A function that creates and initialises a new student node
 // and returns a pointer to it
 struct student *create_student(int zID, double mark, char *name) {
-    struct student *new_student = malloc(sizeof(struct student));
-    new_student->zID = zID;
-    new_student->mark = mark;
-    strcpy(new_student->name, name);
-    return new_student;
+    struct student *new_student_ptr = malloc(sizeof(struct student));
+    new_student_ptr->zID = zID;
+    new_student_ptr->mark = mark;
+    strcpy(new_student_ptr->name, name);
+    new_student_ptr->next_student = NULL;
+    return new_student_ptr;
 }
--- W09B/tut08/malloc.c
+++ W09B/tut08/malloc.c
@@ -10,19 +10,24 @@
 struct student *create_student(int zID, double mark, char *name);
 
 struct student {
-    int zID;
+    int zID;    
     double mark;
-    char name[MAX_NAME_LENGTH];
-}
+    char name[MAX_NAME_LENGTH]; 
+};
 
 int main(void) {
 
+    struct student *frankie = create_student(5, 1000, "frankie");
+    printf("the zID of frankie is: %d\n", frankie->zID);
 
     return 0;
 }
 
-// A function that creates a new struct student
-// and returns a pointer to it
+// A function that creates a new struct student and returns a pointer to it
 struct student *create_student(int zID, double mark, char *name) {
-
+    struct student *new_student_ptr = malloc(sizeof(struct student));
+    new_student_ptr->zID = zID;
+    new_student_ptr->mark = mark;
+    strcpy(new_student_ptr->name, name);
+    return new_student_ptr;
 }
